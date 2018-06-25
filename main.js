9f62ee58c568baf6b34a5dacd394733891550230
Chris <chrisvass1@gmail.com>
2018-06-25 19:22:50 -0400

    added paddle functionality

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
+var mousex;
+var mousey;
+document.addEventListener("mousemove", function (e) {
+  mousex = e.clientX;
+  mousey = e.clientY;
+})
+
 // returns random inclusive integers
 function rand(min, max) {
   min = Math.ceil(min);
@@ -18,16 +25,23 @@ function paddle(x,y,l,h,color){
   this.h = h;
   this.color = color;
   this.update = function(){
+
+    //move paddle to mouse location
+    this.y = mousey-this.h/2;
+
+    //draw paddle
+    this.draw();
   }
+
   this.draw = function() {
-    ctx.fillStyle(this.color);
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.x,this.y,this.l,this.h)
   }
 }
 
 
 //create ball
-function ball(x,y,size,dx,dy,color) {
+function Ball(x,y,size,dx,dy,color) {
   this.x = x;
   this.y = y;
   this.dx = dy;
@@ -43,6 +57,14 @@ function ball(x,y,size,dx,dy,color) {
 
   //update function (includes collision)
   this.update = function () {
+
+    //if in paddle, reverse x velocity
+    for (paddle of paddles) {
+      if (this.x+this.size>paddle.x && this.x<paddle.x+paddle.l && this.y<paddle.y+paddle.h && this.y+this.size>paddle.y) {
+        this.dx = -this.dx
+      }
+    }
+
     //if on edge, reverse velocity
     if (this.y<0 || this.y + this.size > c.height){
       this.dy = -this.dy;
@@ -52,8 +74,8 @@ function ball(x,y,size,dx,dy,color) {
     if (this.x<0 || this.x + this.size >c.width || this.dx == 0) {
       this.x = c.width/2;
       this.y = c.height/2;
-      this.dx = rand(0,1) ? 3 : -3; // TODO: make it not just integer
-      this.dy = rand(-4,4);
+      this.dx = rand(0,1) ? rand(3,6) : rand(-6,-3); // TODO: make it not just integer
+      this.dy = rand(-400,400)/50;
     }
 
     //update velocity
@@ -65,6 +87,10 @@ function ball(x,y,size,dx,dy,color) {
   }
 }
 
+//create paddles
+var paddles = [];
+paddles.push(new paddle(450,c.width/2,20,70,"green"));
+paddles.push(new paddle(30,c.width/2,20,70,"red"));
 
 //create ball
 var ball = new ball(5,5,10,0,0,'blue');
@@ -72,6 +98,9 @@ var ball = new ball(5,5,10,0,0,'blue');
 //game loop
 function loop() {
   ctx.clearRect(0,0,c.height,c.width)
+  for (paddle of paddles) {
+    paddle.update();
+  }
   ball.update();
 }
 setInterval(loop, 20);
